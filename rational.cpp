6f35c53b73a3f9017e8e757534626115e70d4222
nkhaldi <mnk80898@gmail.com>
2023-03-30 16:25:41 +0300

    added operators + - * /

--- rational.cpp
+++ rational.cpp
@@ -3,12 +3,11 @@
  * В нем определены методы add, sub, mul и div, которые прибавляют к нему число,
  * отнимают число, умножают на число и делят на число соответственно.
  * Кроме того в нем определен метод neg, который меняет знак на противоположный.
- * 
+ *
  * Вам нужно определить операторы +=, -=, *=, /= для класса Rational так,
  * чтобы они могли принимать в качестве аргументов и объекты типа Rational и целые числа.
- * Кроме того вам необходимо определить операторы унарного минуса и плюса для класса Rational. 
-*/
-
+ * Кроме того вам необходимо определить операторы унарного минуса и плюса для класса Rational.
+ */
 
 struct Rational
 {
@@ -22,14 +21,113 @@ struct Rational
     void neg();
     void inv();
     double to_double() const;
-    
-    Rational const operator + () const { return *this; }
-    Rational const operator - () const { return Rational(-numerator_, denominator_); }
 
-    Rational & operator += (Rational const& right ) { this->add(right); return *this; }
-    Rational & operator -= (Rational const& right ) { this->sub(right); return *this; }
-    Rational & operator *= (Rational const& right ) { this->mul(right); return *this; }
-    Rational & operator /= (Rational const& right ) { this->div(right); return *this; }
+    Rational const operator+() const { return *this; }
+    Rational const operator-() const { return Rational(-numerator_, denominator_); }
+
+    Rational operator+(const Rational &rational) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.add(rational);
+        return r;
+    }
+    Rational operator+(int d) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.add(Rational(d));
+        return r;
+    }
+    friend Rational operator+(int d, const Rational &rational)
+    {
+        return rational + d;
+    }
+
+    Rational operator-(const Rational &rational) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.sub(rational);
+        return r;
+    }
+    Rational operator-(double d) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.sub(Rational(d));
+        return r;
+    }
+    friend Rational operator-(int d, const Rational &rational)
+    {
+        return -(rational - d);
+    }
+
+    Rational operator*(const Rational &rational) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.mul(rational);
+        return r;
+    }
+    Rational operator*(int d) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.mul(Rational(d));
+        return r;
+    }
+    friend Rational operator*(int d, const Rational &rational)
+    {
+        return rational * d;
+    }
+
+    Rational operator/(const Rational &rational) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.div(rational);
+        return r;
+    }
+    Rational operator/(int d) const
+    {
+        Rational r(this->numerator_, this->denominator_);
+        r.div(Rational(d));
+        return r;
+    }
+    friend Rational operator/(int d, const Rational &rational)
+    {
+        return Rational(d) / rational;
+    }
+
+    Rational & operator+=(const Rational & rational) {
+        this->add(rational);
+        return *this;
+    }
+    Rational & operator+=(int d) {
+        this->add(Rational(d));
+        return *this;
+    }
+    
+    Rational & operator-=(const Rational & rational) {
+        this->sub(rational);
+        return *this;
+    }
+    Rational & operator-=(int d) {
+        this->sub(Rational(d));
+        return *this;
+    }
+
+    Rational & operator*=(const Rational & rational) {
+        this->mul(rational);
+        return *this;
+    }
+    Rational & operator*=(int d) {
+        this->mul(Rational(d));
+        return *this;
+    }
+
+    Rational & operator/=(const Rational & rational) {
+        this->div(rational);
+        return *this;
+    }
+    Rational & operator/=(int d) {
+        this->div(Rational(d));
+        return *this;
+    }
 
 private:
     int numerator_;
