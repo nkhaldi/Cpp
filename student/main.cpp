b5f6bd718188b1ee8361b5cca9295d049ec7643b
nkhaldi <mnk80898@gmail.com>
2023-04-03 16:34:34 +0300

    replaced obj ptr to pbj

--- student/main.cpp
+++ student/main.cpp
@@ -2,20 +2,20 @@
 
 int main()
 {
-    Student *me = new Student("Narek", 24);
+    Student me("Narek", 24);
 
-    me->set_exam("math", 4);
-    me->set_exam("informatics", 5);
-    me->set_exam("literature", 5);
-    me->set_exam("chemestry", 3);
+    me.set_exam("math", 4);
+    me.set_exam("informatics", 5);
+    me.set_exam("literature", 5);
+    me.set_exam("chemestry", 3);
 
     cout << "Solo getter" << endl;
-    cout << "  math: " << me->get_exam("math") << endl;
-    cout << "  informatics: " << me->get_exam("informatics") << endl;
+    cout << "  math: " << me.get_exam("math") << endl;
+    cout << "  informatics: " << me.get_exam("informatics") << endl;
     cout << endl;
 
     cout << "Full getter" << endl;
-    exam_map all_exams = me->get_all_exams();
+    exam_map all_exams = me.get_all_exams();
     for (auto it : all_exams)
     {
         cout << "  " << it.first << ": " << it.second << endl;
